41108df98d4e73d8dcb8a39c7dd79733c7dc1cdb
Matei Dibu <matdibu@protonmail.com>
2022-07-28 22:06:41 +0300

    improved logging

--- run_fixed.sh
+++ run_fixed.sh
@@ -1,4 +1,10 @@
-#!/bin/sh
+#!/bin/bash
+
+RED="\e[31m"
+GREEN="\e[32m"
+ENDCOLOR="\e[0m"
+
+trap 'if [[ $? -eq 139 ]]; then echo -e "${RED}segfault!${ENDCOLOR}"; fi' CHLD
 
 TARGETS="segfault fixed"
 COMPILERS="gcc clang"
@@ -7,8 +13,17 @@ COMPILE_OPTIONS="-g -Og -O0 -O1 -O2 -O3"
 for target in $TARGETS; do
 	for compiler in $COMPILERS; do
 		for variant in $COMPILE_OPTIONS; do
-			echo "running $target with $compiler $variant"
-			$compiler -m64 $variant $target.c -o $target && ./$target
+			compile_command="$compiler -m64 $variant $target.c"
+			echo -en "$compile_command: "
+			$compile_command
+			output=$(./a.out 2>&1)
+			returnval=$?
+			unique_lines=$(echo "$output" | sort -u | wc -l)
+			if [[ $returnval != 139 ]] && [[ $unique_lines -eq 1 ]]; then
+				echo -e "${GREEN}success!${ENDCOLOR}"
+			elif [ ! -z "$output" ]; then
+				echo -e "${RED}different addresses:${ENDCOLOR}\n$output"
+			fi
 		done
 	done
 done
